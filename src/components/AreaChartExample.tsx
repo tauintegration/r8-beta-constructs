5aeffeb3472d1b5d50743594629f912af1350c9b
PC <choipco@gmail.com>
2024-01-25 13:52:11 -0700

    - fixed client.js issue and tabbing for dashboard in place with stubbed examples of charts  -- line for time-series   --- need buttons to change the data set   --- need dropdown to change the data set  -- dimensional   --- separate (and replicate interactive features)  -- don't forget responsiveness on mobile

---
+++ src/components/AreaChartExample.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { Chance } from 'chance';
+
+const chance = new Chance();
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: 'Dataset 2',
+      data: labels.map(() => chance.integer({ min: -1000, max: 1000 })),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export default function AreaChartExample() {
+  return <Line options={options} data={data} />;
+}
